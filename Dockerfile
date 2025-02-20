f60ba7bfe3aa71f18b7a74be6c0fcd4fa23a0805
mjkang4416 <mjkang4416@naver.com>
2025-02-20 11:33:19 +0000

    [docker]docker

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM krmp-d2hub-idock.9rum.cc/goorm/mariadb:10.6.12
+FROM krmp-d2hub-idock.9rum.cc/goorm/mysql:8.0.39
--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,16 +1,22 @@
-CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA IF NOT EXISTS `AssistPeople` DEFAULT CHARACTER SET utf8mb4;
 
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
+USE `AssistPeople`;
 
-USE `krampoline`;
+DROP TABLE IF EXISTS `user`;
 
-DROP TABLE IF EXISTS `sample_data`;
-CREATE TABLE `sample_data` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `detail` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `user` (
+    `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `nick_name` VARCHAR(50) UNIQUE,
+    `password` VARCHAR(255),
+    `mbti` VARCHAR(10),
+    `id` VARCHAR(50) UNIQUE,
+    `introduce` TEXT,
+    `reason` TEXT,
+    `appeal` TEXT,
+    `user_pic` VARCHAR(255),
+    `role` VARCHAR(20),
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
+INSERT INTO `user` (`nick_name`, `password`, `mbti`, `id`, `introduce`, `reason`, `appeal`, `user_pic`, `role`) 
+VALUES ('testUser', 'password123', 'INFJ', 'testID', 'Hello! I am a user.', 'I love coding.', 'Hardworking person.', 'profile.jpg', 'USER');
